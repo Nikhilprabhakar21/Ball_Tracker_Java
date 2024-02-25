395a98d69c23efd806b68d927be678c7d53e5a24
LordNP_Overlord <nikhil@aprabhakar.com>
2024-02-25 11:33:35 -0800

    sadfasddf

--- src/Filters/BallTrackingRGB.java
+++ src/Filters/BallTrackingRGB.java
@@ -21,32 +21,16 @@ public class BallTrackingRGB implements PixelFilter, Interactive {
         short[][] green = img.getGreenChannel();
         short[][] blue = img.getBlueChannel();
 
-        short[][] red1 = img.getRedChannel();
-        short[][] green1 = img.getGreenChannel();
-        short[][] blue1 = img.getBlueChannel();
-
         ArrayList<Point> maskedColors = new ArrayList<>();
 
         doMask(red, green, blue, maskedColors);
         Point middle = calcMidP(maskedColors);
         makeSquare(middle.getRow(), middle.getColumn(), red, green, blue, 5);
-        //bringImageBack(red, green, blue, red1, green1, blue1);
 
         img.setColorChannels(red, green, blue);
         return img;
     }
 
-    public void bringImageBack(short[][] red, short[][] green, short[][] blue, short[][] redO, short[][] greenO, short[][] blueO){
-        for (int r = 0; r < red.length; r++) {
-            for (int c = 0; c < red[0].length; c++) {
-                if (red[r][c] == blue[r][c] && blue[r][c] == green[r][c]){
-                    red[r][c] = redO[r][c];
-                    blue[r][c]  = blueO[r][c];
-                    green[r][c] = greenO[r][c];
-                }
-            }
-        }
-    }
 
     public void makeSquare(int r, int c, short[][] red, short[][] green, short[][] blue, int size){
 
